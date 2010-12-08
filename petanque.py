dde452e8cb10a5fab4449bbcd998b22e73b19372
pierre gueth <pierre.gueth@creatis.insa-lyon.fr>
2010-12-08 16:24:22 +0100

    matrix projections

---
+++ petanque.py
@@ -0,0 +1,21 @@
+__all__ = ["project_points"]
+
+from scipy import *
+
+def cart_from_sphere(pointsphere):
+    assert(pointsphere[0]>0)
+    assert(pointsphere[2]>=0 and pointsphere[2]<=pi/2)
+    return pointsphere[0]*array([sin(pointsphere[1])*sin(pointsphere[2]),-cos(pointsphere[1])*sin(pointsphere[2]),cos(pointsphere[2])])
+
+def norm_cart_from_sphere(pointsphere):
+    return cart_from_sphere((1,pointsphere[1],pointsphere[2]))
+
+def project_points(pointsplan,cameraplan,camerasphere):
+    cameraouter = cart_from_sphere(camerasphere)
+    cameraouter_norm = norm_cart_from_sphere(camerasphere)
+    cameraup = cart_from_sphere([1,pi+camerasphere[1],pi/2-camerasphere[2]])
+    assert(abs(dot(cameraouter_norm,cameraup))<10e-5)
+    rot = array([cross(cameraup,cameraouter_norm),cameraup,cameraouter_norm])
+    points = dot(rot,vstack([pointsplan[0,:]-cameraplan[0]-cameraouter[0],pointsplan[1,:]-cameraplan[1]-cameraouter[1],zeros(pointsplan.shape[1])-cameraouter[2]]))
+    assert((points[2,:]<0).all())
+    return -4.*points[:2,:]/points[2,:]
--- projection.py
+++ projection.py
@@ -3,6 +3,7 @@
 
 from pylab import *
 import scipy.linalg as lin
+import petanque
 
 def display_grid(mx,my):
     figure()
@@ -17,35 +18,13 @@ def display_grid(mx,my):
     ylim(-20,20)
     xlim(-20,20)
 
-def cart_from_sphere(pointsphere):
-    assert(pointsphere[0]>0)
-    assert(pointsphere[2]>=0 and pointsphere[2]<=pi/2)
-    return pointsphere[0]*array([sin(pointsphere[1])*sin(pointsphere[2]),-cos(pointsphere[1])*sin(pointsphere[2]),cos(pointsphere[2])])
-
-def norm_cart_from_sphere(pointsphere):
-    return cart_from_sphere((1,pointsphere[1],pointsphere[2]))
-
-def project_point_internal(pointplan,cameraplan,cameraouter,rot):
-    point = dot(rot,[pointplan[0]-cameraplan[0],pointplan[1]-cameraplan[1],0]-cameraouter)
-    assert(point[2]<0)
-    return -4.*point[:2]/point[2]
-
-def project_point(pointplan,cameraplan,camerasphere):
-    cameraouter = cart_from_sphere(camerasphere)
-    cameraouter_norm = norm_cart_from_sphere(camerasphere)
-    cameraup = cart_from_sphere([1,pi+camerasphere[1],pi/2-camerasphere[2]])
-    assert(abs(dot(cameraouter_norm,cameraup))<10e-5)
-    rot = array([cross(cameraup,cameraouter_norm),cameraup,cameraouter_norm])
-    return project_point_internal(pointplan,cameraplan,cameraouter,rot)
-
 def project_grid(mx,my,cameraplan,camerasphere):
     nmx = zeros(mx.shape)
     nmy = zeros(my.shape)
-    for ii in xrange(mx.shape[0]):
-        for jj in xrange(mx.shape[1]):
-            nmx[ii,jj],nmy[ii,jj] = project_point((mx[ii,jj],my[ii,jj]),cameraplan,camerasphere)
-    return nmx,nmy
-
+    foo = petanque.project_points(vstack((mx.ravel(),my.ravel())),cameraplan,camerasphere)
+    mx = foo[0,:].reshape(nmx.shape)
+    my = foo[1,:].reshape(nmy.shape)
+    return mx,my
 
 omx,omy = meshgrid(arange(-5,6),arange(-5,6))
 nmx,nmy = project_grid(omx,omy,array((0,0)),array((4,0,0)))
