6f079cd5aa2b87281e4b8d6f655ef4d119f26ca2
elcerdo <georges.wbushjr@laposte.net>
2010-12-10 17:10:25 +0100

    added noise to projections

--- simulation.py
+++ simulation.py
@@ -52,23 +52,36 @@ for theta in linspace(0,2*pi,10,endpoint=False):
     projections_truth = petanque.project_points(positions_truth,cameraplan,camerasphere,camerafocal)
     datas.append(((cameraplan,camerasphere,camerafocal),projections_truth))
 
+datas_noisy = [(params,projections+randn(projections.size).reshape(projections.shape)*.001) for params,projections in datas]
+
 print "estimating positions with fixed camera params"
 positions_estimated = petanque.estimate_positions(datas)
 print "bias=%s [mm]" % (1e3*(positions_estimated-positions_truth).mean(axis=1))
 print "std=%s [mm]" % (1e3*(positions_estimated-positions_truth).std(axis=1))
 
+print "estimating positions with fixed camera params with noisy projections"
+positions_estimated_noisy = petanque.estimate_positions(datas_noisy)
+print "bias=%s [mm]" % (1e3*(positions_estimated_noisy-positions_truth).mean(axis=1))
+print "std=%s [mm]" % (1e3*(positions_estimated_noisy-positions_truth).std(axis=1))
+
 print "display results"
 factor = display_grid(mx,my,2)
-plot(factor*positions_truth[0,:],factor*positions_truth[1,:],'go-')
-plot(factor*positions_estimated[0,:],factor*positions_estimated[1,:],'r*-')
+plot(factor*positions_truth[0,:],factor*positions_truth[1,:],'go',label="truth")
+plot(factor*positions_estimated[0,:],factor*positions_estimated[1,:],'g*',label="est")
+plot(factor*positions_estimated_noisy[0,:],factor*positions_estimated_noisy[1,:],'r*',label="est/noise")
+legend()
 title('plan de la table')
 
-for kk,((cameraplan,camerasphere,camerafocal),projections_truth) in enumerate(datas):
+for kk,(((cameraplan,camerasphere,camerafocal),projections_truth),(params2,projections_noisy)) in enumerate(zip(datas,datas_noisy)):
     projections_estimated = petanque.project_points(positions_estimated,cameraplan,camerasphere,camerafocal)
+    projections_estimated_noisy = petanque.project_points(positions_estimated_noisy,cameraplan,camerasphere,camerafocal)
     projections_mx,projections_my = project_grid(mx,my,cameraplan,camerasphere,camerafocal)
     factor = display_grid(projections_mx,projections_my,.05)
-    plot(factor*projections_truth[0,:],factor*projections_truth[1,:],'go-')
-    plot(factor*projections_estimated[0,:],factor*projections_estimated[1,:],'r*-')
+    plot(factor*projections_truth[0,:],factor*projections_truth[1,:],'go',label="truth")
+    plot(factor*projections_estimated[0,:],factor*projections_estimated[1,:],'g*',label="est")
+    plot(factor*projections_noisy[0,:],factor*projections_noisy[1,:],'ro',label="noise")
+    plot(factor*projections_estimated_noisy[0,:],factor*projections_estimated_noisy[1,:],'r*',label="est/noise")
+    legend()
     title(u"config %d plan=(%.2f,%.2f) r=%.2f theta=%.0f° phi=%.0f°" % (kk,cameraplan[0],cameraplan[1],camerasphere[0],180/pi*camerasphere[1],180/pi*camerasphere[2]))
 
 show()
