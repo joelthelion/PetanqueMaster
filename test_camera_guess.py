107ac6fd847ff24d0a02bebb021b60cd9714badb
pierre gueth <pierre.gueth@creatis.insa-lyon.fr>
2010-12-13 15:35:34 +0100

    guess elevation and distance

--- test_camera_guess.py
+++ test_camera_guess.py
@@ -6,7 +6,7 @@ import petanque
 
 camerafocal = 0.05
 standard_ball_radius = .09
-number_of_balls = 10
+number_of_balls = 5
 number_of_pictures = 1000
 
 mx,my = meshgrid(linspace(-1.5,1.5,7),linspace(-1.5,1.5,7))
@@ -15,15 +15,30 @@ positions = vstack((positions,standard_ball_radius*ones(positions.shape[1])))
 factor = petanque.display_grid(mx,my,4)
 petanque.display_projections(factor,positions,"ro",None)
 
+def guess_camera_params(projections):
+    (ar,br) = polyfit(projections[1,:],projections[2,:],1)
+    distance = standard_ball_radius*camerafocal/br
+    elevation = arctan(-ar/br*camerafocal)
+    return (distance,elevation)
+
 distances = []
 phis = []
 for kk in xrange(number_of_pictures):
-    cameraplan = .2*randn(2)
-    camerasphere = array((1.5+random()*2,2*pi*random(),pi/180*(5.+random()*30)))
+    cameraplan = .9*randn(2)
+    camerasphere = array((1+random()*3,2*pi*random(),pi/180*(random()*30)))
     projections = petanque.project_points(positions,cameraplan,camerasphere,camerafocal)
 
-    distances.append((camerasphere[0],projections[2,:].mean()))
-    phis.append((camerasphere[2],projections[2,:].std()/projections[2,:].mean()))
+    params = guess_camera_params(projections)
+    distances.append((camerasphere[0],params[0]))
+    phis.append((camerasphere[2],params[1]))
+
+    #figure()
+    #(ar,br) = polyfit(projections[1,:],projections[2,:],1)
+    #plot(projections[1,:],projections[2,:],'+')
+    #print br,projections[2,:].mean()
+    #xs = linspace(-.05,.05,50)
+    #plot(xs,polyval((ar,br),xs))
+    #plot(xs,ar*xs+br,':')
 
     #projections_mx,projections_my = petanque.project_grid(mx,my,cameraplan,camerasphere,camerafocal)
     #factor = petanque.display_grid(projections_mx,projections_my,.05)
@@ -31,19 +46,21 @@ for kk in xrange(number_of_pictures):
     #title(u"config %d plan=(%.2f,%.2f) r=%.2f theta=%.0f° phi=%.0f°" % (kk,cameraplan[0],cameraplan[1],camerasphere[0],180/pi*camerasphere[1],180/pi*camerasphere[2]))
 
 distances = array(distances)
-xdistances = linspace(1.5,3.5,256)
+xdistances = linspace(.5,3.5,256)
 
 figure()
-plot(distances[:,0],1e3*distances[:,1],'+')
-plot(xdistances,1e3*standard_ball_radius*camerafocal/xdistances,'-')
+plot(distances[:,0],distances[:,1],'+')
+plot(xdistances,xdistances,'-')
 xlabel("camera distance [m]")
-ylabel("mean balls size [cm]")
+ylabel("guessed distance [m]")
 
 phis = array(phis)
+xphis = linspace(0,35,256)*pi/180
 
 figure()
-plot(180/pi*phis[:,0],1e3*phis[:,1],'+')
+plot(180/pi*phis[:,0],180/pi*phis[:,1],'+')
+plot(180/pi*xphis,180/pi*xphis,'-')
 xlabel(u"camera elevation [°]")
-ylabel("std/mean balls size [na]")
+ylabel(u"guessed elevation [°]")
 
 show()
